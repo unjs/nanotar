6fa56df81d68665ec13b3f4afd24d4a7199dcf76
Pooya Parsa <pooya@pi0.io>
2025-01-21 14:17:42 +0200

    test: update fixture
    #29

--- test/fixtures/gen.bash
+++ test/fixtures/gen.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# set -xe
 
 cd "$(dirname "$0")" || exit 1
 
@@ -7,6 +8,7 @@ create_tar() {
     local format=$1
     local ext=$2
     local options=$3
+    local exclude=$4
     tar $options -cf "out/${format}.tar" -C in .
     if [[ $? -eq 0 ]]; then
         echo "Created: out/${format}.tar"
@@ -15,18 +17,18 @@ create_tar() {
     fi
 }
 
+# Cleanup
+
+rm out/*.tar
+
 # V7 (Original TAR format)
-create_tar "v7" "tar" "--format=v7"
+create_tar "v7" "tar" "--format=v7" "long"
 
 # USTAR (POSIX 1988)
-create_tar "ustar" "tar" "--format=ustar"
+create_tar "ustar" "tar" "--format=ustar" "long"
 
 # GNU TAR (Linux standard)
-create_tar "gnu" "tar" "--format=gnu"
+create_tar "gnu" "tar" "--format=gnu" ""
 
 # PAX TAR (POSIX 2001)
-create_tar "pax" "tar" "--format=pax"
-
-# STAR TAR (Schily TAR - not always available, fallback to pax)
-create_tar "star" "tar" "--format=star"
-
+create_tar "pax" "tar" "--format=pax" ""
